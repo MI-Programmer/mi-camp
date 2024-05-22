330aa0051bf5740cc9825ccfc645915e2f170d6d
MI-Progammer <ilhamdivine7@gmail.com>
2024-05-22 11:44:12 +0700

    Add expires session and max age session

--- app.js
+++ app.js
@@ -64,10 +64,10 @@ const sessionConfig = {
   resave: false,
   saveUninitialized: true,
   cookie: {
-    // secure: true,
+    secure: true,
     httpOnly: true,
-    // expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    // maxAge: 1000 * 60 * 60 * 24 * 7,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
 app.use(session(sessionConfig));
