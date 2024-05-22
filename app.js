c55a27c415926fac0f962ac1847f47ec719d39a1
MI-Progammer <ilhamdivine7@gmail.com>
2024-05-22 17:33:37 +0700

    add permission for bootstrap

--- app.js
+++ app.js
@@ -89,6 +89,7 @@ const scriptSrcUrls = [
   "https://cdn.jsdelivr.net",
 ];
 const styleSrcUrls = [
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
   "https://kit-free.fontawesome.com",
   "https://stackpath.bootstrapcdn.com",
   "https://api.mapbox.com",
